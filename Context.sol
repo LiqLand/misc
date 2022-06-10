0c6209e84de002abe9decae6bdb658eeedc39933
LiqLand <104589983+LiqLand@users.noreply.github.com>
2022-06-09 20:45:18 -0700

    Update Context.sol

--- Context.sol
+++ Context.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
 abstract contract Context {
     function _msgSender() internal view virtual returns (address) {return msg.sender;}
     function _msgData() internal view virtual returns (bytes calldata) {this; return msg.data;}
