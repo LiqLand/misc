c4aba5a84433161652247502f8e859889d8ed4b1
LiqLand <104589983+LiqLand@users.noreply.github.com>
2022-06-09 20:19:35 -0700

    Create Ownable.sol

---
+++ Ownable.sol
@@ -0,0 +1,42 @@
+abstract contract Ownable is Context {
+    address public _owner;
+    address public _previousOwner;
+    uint256 private _lockTime;
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+    constructor () {
+        address msgSender = _msgSender();
+        _owner = msgSender;
+        emit OwnershipTransferred(address(0), msgSender);
+    }
+    function owner() public view returns (address) {
+        return _owner;
+    }
+    modifier onlyOwner() {
+        require(_owner == _msgSender(), "Ownable: caller is not the owner");
+        _;
+    }
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        emit OwnershipTransferred(_owner, newOwner);
+        _owner = newOwner;
+    }
+}
+
+abstract contract Manageable is Context {
+    address public _manager;
+    event ManagementTransferred(address indexed previousManager, address indexed newManager);
+    constructor(){
+        address msgSender = _msgSender();
+        _manager = msgSender;
+        emit ManagementTransferred(address(0), msgSender);
+    }
+    function manager() public view returns(address){ return _manager; }
+    modifier onlyManager(){
+        require(_manager == _msgSender(), "Manageable: caller is not the manager");
+        _;
+    }
+    function transferManagement(address newManager) external virtual onlyManager {
+        emit ManagementTransferred(_manager, newManager);
+        _manager = newManager;
+    }
+}
