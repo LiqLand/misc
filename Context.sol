fa5b2cb6ffe59cafe17767cde143afc3283d66d0
LiqLand <104589983+LiqLand@users.noreply.github.com>
2022-06-09 20:18:18 -0700

    Create Context.sol

---
+++ Context.sol
@@ -0,0 +1,14 @@
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {return msg.sender;}
+    function _msgData() internal view virtual returns (bytes calldata) {this; return msg.data;}
+}
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256) {return a + b;}
+    function sub(uint256 a, uint256 b) internal pure returns (uint256) {return a - b;}
+    function mul(uint256 a, uint256 b) internal pure returns (uint256) {return a * b;}
+    function div(uint256 a, uint256 b) internal pure returns (uint256) {return a / b;}
+    function mod(uint256 a, uint256 b) internal pure returns (uint256) {return a % b;}
+    function sub(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
+        unchecked { require(b <= a, errorMessage); return a - b; }
+    }
+}
