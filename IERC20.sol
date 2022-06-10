c2d9caf20afd9b065e777aec670f4345ace6c188
LiqLand <104589983+LiqLand@users.noreply.github.com>
2022-06-09 20:45:58 -0700

    Update IERC20.sol

--- IERC20.sol
+++ IERC20.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
 interface IERC20 {
     function totalSupply() external view returns (uint256);
     function balanceOf(address account) external view returns (uint256);
