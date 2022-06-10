bedea3768ae06a1089fbf64b8dc39fe4d621ff1a
LiqLand <104589983+LiqLand@users.noreply.github.com>
2022-06-09 20:44:51 -0700

    Update Address.sol

--- Address.sol
+++ Address.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
 library Address {
     function isContract(address account) internal view returns (bool) { uint256 size; assembly { size := extcodesize(account) } return size > 0;}
     function sendValue(address payable recipient, uint256 amount) internal {
