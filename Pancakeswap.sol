74bf1e3c5dc04b35cbed2193929efee9447f6189
LiqLand <104589983+LiqLand@users.noreply.github.com>
2022-06-09 20:46:43 -0700

    Update Pancakeswap.sol

--- Pancakeswap.sol
+++ Pancakeswap.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.8;
 interface IPancakeV2Factory {
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
     function createPair(address tokenA, address tokenB) external returns (address pair);
