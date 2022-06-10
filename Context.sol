41a6d601c7b6cf5a28cd56026b250e9bc8762e10
LiqLand <104589983+LiqLand@users.noreply.github.com>
2022-06-09 20:33:27 -0700

    Update Context.sol

--- Context.sol
+++ Context.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 abstract contract Context {
     function _msgSender() internal view virtual returns (address) {return msg.sender;}
     function _msgData() internal view virtual returns (bytes calldata) {this; return msg.data;}
