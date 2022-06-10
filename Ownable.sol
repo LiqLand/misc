07664889b45465da77ecf32b7508cdc3cbc1f9ab
LiqLand <104589983+LiqLand@users.noreply.github.com>
2022-06-09 20:40:58 -0700

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -22,22 +22,3 @@ abstract contract Ownable is Context {
         _owner = newOwner;
     }
 }
-
-abstract contract Manageable is Context {
-    address public _manager;
-    event ManagementTransferred(address indexed previousManager, address indexed newManager);
-    constructor(){
-        address msgSender = _msgSender();
-        _manager = msgSender;
-        emit ManagementTransferred(address(0), msgSender);
-    }
-    function manager() public view returns(address){ return _manager; }
-    modifier onlyManager(){
-        require(_manager == _msgSender(), "Manageable: caller is not the manager");
-        _;
-    }
-    function transferManagement(address newManager) external virtual onlyManager {
-        emit ManagementTransferred(_manager, newManager);
-        _manager = newManager;
-    }
-}
