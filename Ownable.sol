31a1fdb794aff68929f6528fad3099c64d517360
LiqLand <104589983+LiqLand@users.noreply.github.com>
2022-07-18 21:01:15 -0700

    Update Ownable.sol

--- Ownable.sol
+++ Ownable.sol
@@ -2,7 +2,15 @@
 // OpenZeppelin Contracts (last updated v4.7.0) (access/Ownable.sol)
 
 pragma solidity ^0.8.0;
-import "../Context.sol";
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+    function _msgData() internal view virtual returns (bytes calldata) {
+        this; 
+        return msg.data;
+    }
+}
 contract Ownable is Context {
     address private _owner;
     event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
